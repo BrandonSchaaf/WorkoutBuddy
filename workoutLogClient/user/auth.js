4ac0440a91d362d3164f92ba0ae74d69e5f85d65
Brandon Schaaf <brandonschaaf@gmail.com>
2017-09-06 12:07:41 -0400

    sign in now able to add user

---
+++ workoutLogClient/user/auth.js
@@ -0,0 +1,43 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		// signup method
+		signup: function(){
+			// username & password variables
+			var username = $("#su_username").val();
+			var password = $("#su_password").val();
+			// user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			// signup post
+			var signup = $.ajax({
+				type:"POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+
+			// signup done/fail
+			signup.done(function(data){
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+					console.log("Hey! You did it! You're signed in!");
+					console.log(data.sessionToken);
+				}
+
+				$("#signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+				
+			}).fail(function(){
+				$("#su_error").text("Hmm... Something happened with your sign up.").show();
+			});
+		}
+	});
+		// bind events
+		$("#signup").on("click", WorkoutLog.signup);
+});
