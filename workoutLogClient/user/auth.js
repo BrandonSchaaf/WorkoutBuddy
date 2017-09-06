fc37c0ae5cd39f68d6a5efa30985a70b7fbd42db
Brandon Schaaf <brandonschaaf@gmail.com>
2017-09-06 13:27:09 -0400

    login functional

--- workoutLogClient/user/auth.js
+++ workoutLogClient/user/auth.js
@@ -36,8 +36,52 @@ $(function(){
 			}).fail(function(){
 				$("#su_error").text("Hmm... Something happened with your sign up.").show();
 			});
-		}
+		},
+
+		// login
+
+		login: function() {
+			var username = $("#li_username").val();
+			var password = $("#li_password").val();
+			var user = {user: {username: username, password: password }}
+			var login = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "login",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+
+			login
+			.done(function(data) {
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+					console.log(data.sessionToken);
+				}
+
+				$("#login-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Lougout");
+			})
+
+			.fail(function(){
+				$("#li_error").text("Huh... Something happened during sign up. ")
+			});
+		},
+
+		loginout:function(){
+			if (window.localStorage.getItem("sessionToken")){
+				window.localStorage.removeItem("sessionToken");
+				$("#loginout").text("Login");
+			}
+		} 			// To Do: Make sure stuff is disabled on logout
 	});
-		// bind events
-		$("#signup").on("click", WorkoutLog.signup);
+
+	// bind events
+	$("#login").on("click", WorkoutLog.login);
+	$("#signup").on("click", WorkoutLog.signup);
+	$("#loginout").on("click", WorkoutLog.loginout);
+
+	if (window.localStorage.getItem("sessionToken")) {
+		$("#loginout").text("Logout");
+	}
 });
