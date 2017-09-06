c54a844f59e535d454fc8f506913a6068df1be12
Brandon Schaaf <brandonschaaf@gmail.com>
2017-09-06 09:47:19 -0400

    added more security

--- workoutLogServer/app.js
+++ workoutLogServer/app.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db.js');
-
 var User = sequelize.import('./models/user');
+
 User.sync(); // sync({force:true}) WARNING: This will drop the table!
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/login', require('./routes/session'));
 app.use('/api/test', function(req, res){
